d8ecbdf4d3de34f6f3ddb15953e216afb56b4811
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-05-18 17:41:04 +0900

    Title: 청소당번 정하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240518/청소당번 정하기/cleaning-numbering.py
+++ 240518/청소당번 정하기/cleaning-numbering.py
@@ -5,33 +5,18 @@ way = 0
 wc = 0
 
 for i in range(1, n+1):
-    if(i % 6 == 0 or i % 3 == 0):
-        way += 1
-    
+
     if(i % 2 == 0):
-        if(i % 12 == 0):
+        if(i % 3 == 0):
+            if(i % 6 == 0):
+                cls -= 1
+            way += 1
+        elif(i % 12 == 0):
             wc += 1
-        else:
-            cls += 1
-    elif(i % 12 == 0):
-        wc += 1
+        cls += 1
+    
+    elif(i % 2 == 1):
+        if(i % 3 == 0):
+            way += 1
 
-print(cls-1, way, wc)
-
-
-# n = int(input())
-
-# cls = 0
-# way = 0
-# wc = 0
-
-# for i in range(1, n+1):
-#     if(i % 2 == 0):
-#         if(i % 6 == 0 or i % 3 == 0):
-#             way += 1
-#         elif(i % 12 == 0):
-#             wc += 1
-#         else:
-#             cls += 1
-
-# print(cls, way, wc)
+    print(cls, way, wc)
