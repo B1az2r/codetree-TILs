70f41f6682044ba041870d036039aca45912707f
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-05-20 22:09:22 +0900

    Title: 3n + 1 수열, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240520/3n + 1 수열/3n-plus-1-sequence.py
+++ 240520/3n + 1 수열/3n-plus-1-sequence.py
@@ -2,6 +2,9 @@ n = int(input())
 cnt = 0
 
 while(True):
+    
+    if(n == 1):
+        break
 
     if(n % 2 == 0):
         n /= 2
@@ -9,7 +12,6 @@ while(True):
     elif(n % 2 == 1):
         n = n * 3 + 1
         cnt += 1   
-    if(n == 1):
-        break
+
 
 print(cnt)
