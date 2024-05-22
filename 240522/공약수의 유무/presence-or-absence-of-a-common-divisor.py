1797bb2cd4cc4edbd1f9adef402404600a2b9792
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-05-22 16:13:50 +0900

    Title: 공약수의 유무, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240522/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,13 +1,16 @@
-a,b = map(int,input().split())
-gong = []
-for i in range(2,1920):
-    if 1920 % i == 0 and 2880 % i ==0:
-        gong.append(i)
-sati = False
-for j in range(len(gong)):
-    if gong[j] >=a and gong[j] <=b:
-        sati = True
-if sati == True:
-    print(1)
+inp = input()
+arr = inp.split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+sat = False
+
+for i in range(a, b+1):
+    if(1920 % i == 0 and 2880 % i == 0):
+        sat = True
+
+if sat == True:
+    print("1")
 else:
-    print(0)
+    print("0")
