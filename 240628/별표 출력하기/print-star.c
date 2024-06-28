f64a121b71ee55183fe8fcf496dcaaadc1a7527c
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-06-28 17:09:32 +0900

    Title: 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240628/별표 출력하기/print-star.c
+++ 240628/별표 출력하기/print-star.c
@@ -2,16 +2,19 @@
 
 int main(){
 
-    for(int i = 0; i <= 2; i++){
-        for(int j = 0; j <= 2 - i; j++){
-            printf("*");
+    int n;
+    scanf("%d", &n);
+
+    for(int i = n; i > 0; i--){
+        for(int j = 0; j < n-i+1; j++){
+            printf("* ");
         }
         printf("\n");
     }
 
-    for(int i = 1; i >= 0; i--){
-        for(int j = 0; j <= 2 - i; j++){
-            printf("*");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n-i-1; j++){
+            printf("* ");
         }
         printf("\n");
     }
