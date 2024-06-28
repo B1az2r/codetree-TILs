f8d5daa3829ddf05eb179222d5cca22b738b1612
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-06-28 16:53:59 +0900

    Title: 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240628/별표 출력하기/print-star.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    for(int i = 0; i <= 2; i++){
+        for(int j = 0; j <= 2 - i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for(int i = 1; i >= 0; i--){
+        for(int j = 0; j <= 2 - i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
