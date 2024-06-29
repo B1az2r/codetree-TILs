bd1785ee717fe91f35aee3ea13c426966774c52a
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-06-29 23:32:50 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240629/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    int sum = 0;
+    int arr[10];
+
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    printf("%d", sum);
+
+    return 0;
+}
