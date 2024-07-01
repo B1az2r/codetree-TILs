c0ff4f6009e637fa65a11fcdefe60f1f9dd36967
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-01 18:50:12 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240701/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    int m;
+    scanf("%d", &m);
+
+    for(int i = 0; i < m; i++){
+        int n;
+        scanf("%d", &n);
+        int cnt = 0;
+
+        while (n != 1){
+            if(n % 2 == 0){
+                n /= 2;
+                cnt++;
+            }
+            else if(n % 2 == 1){
+                n = n*3+1;
+                cnt++;
+            }
+        }
+        printf("%d\n", cnt);
+    }
+    
+
+    return 0;
+}
