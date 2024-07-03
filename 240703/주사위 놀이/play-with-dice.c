6bbe246a8888a9d84487a12440a69e742a118bf1
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-03 15:28:31 +0900

    Title: 주사위 놀이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240703/주사위 놀이/play-with-dice.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int arr[10];
+    int cnt[7] = { 0, };
+
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < 10; i++){
+        cnt[arr[i]]++;
+    }
+
+    for(int i = 1; i < 7; i++){
+        printf("%d - %d\n", i, cnt[i]);
+    }
+
+    return 0;
+}
