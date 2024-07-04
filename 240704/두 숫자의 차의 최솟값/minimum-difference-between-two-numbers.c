70eeaf159855cd80f19ac085aa46a4f2b5ed2a29
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-04 16:18:18 +0900

    Title: 두 숫자의 차의 최솟값, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240704/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.c
+++ 240704/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.c
@@ -8,11 +8,11 @@ int main(){
     int arr[10];
     int min = 100;
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i <= n-1; i++){
         scanf("%d", &arr[i]);
     }
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i <= n-1; i++){
         if(arr[i] != arr[i+1]){
             if(arr[i+1] - arr[i] < min){
                 min = arr[i+1] - arr[i];
