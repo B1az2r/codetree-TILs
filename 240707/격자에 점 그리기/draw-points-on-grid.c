62b7a1b5fcfb5cebfb637fa05f64d2230660a5eb
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-07 16:00:50 +0900

    Title: 격자에 점 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240707/격자에 점 그리기/draw-points-on-grid.c
+++ 240707/격자에 점 그리기/draw-points-on-grid.c
@@ -5,21 +5,22 @@ int main(){
     int n, m;
     scanf("%d %d", &n, &m);
     int arr[10][10] = {0, };
+    int cnt = 1;
 
 
     for(int i = 0; i < m; i++){
         int r, c;
         scanf("%d %d", &r, &c);
-        arr[r][c] = 1;
+        arr[r][c] = cnt;
+        cnt++;
+        
     }
 
-    int cnt = 1;
     
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
-            if(arr[i][j] == 1){
-                printf("%d ", cnt);
-                cnt++;
+            if(arr[i][j] != 0){
+                printf("%d ", arr[i][j]);
             }
             else{
                 printf("0 ");
