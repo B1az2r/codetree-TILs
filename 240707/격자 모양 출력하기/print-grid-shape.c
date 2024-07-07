6c0d911bca0dee21618fbe2e729764c6c28501aa
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-07 17:14:31 +0900

    Title: 격자 모양 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240707/격자 모양 출력하기/print-grid-shape.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int arr[10][10] = {0, };
+    
+
+
+    for(int i = 0; i < m; i++){
+        int r, c;
+        scanf("%d %d", &r, &c);
+        arr[r][c] = r*c;
+        
+    }
+
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(arr[i][j] != 0){
+                printf("%d ", arr[i][j]);
+            }
+            else{
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
