72dd0cde2e569da0f7ae0cad01d2034c28ef99f5
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-07 19:55:16 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240707/단어로 구분하기/separate-words-with-words.c
+++ 240707/단어로 구분하기/separate-words-with-words.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    
-    char arr[300];
-    fgets(arr, 300, stdin);
 
-    for(int i = 0; i < 300; i++){
-        if(arr[i] == ' '){
-            arr[i] = '\n';
-        }
-        if(arr[i] == '\0'){
-            break;
-        }
-        printf("%c", arr[i]);
+    char arr[10][201];
+
+    for(int i = 0; i < 10; i++){
+        scanf("%s", arr[i]);
+        printf("%s\n", arr[i]);
     }
-
 
     return 0;
 }
