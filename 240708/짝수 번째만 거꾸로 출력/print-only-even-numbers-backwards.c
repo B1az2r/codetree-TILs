b0b191301612b87a73823c8308889c86300db4e8
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-08 20:17:37 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240708/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.c
+++ 240708/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.c
@@ -6,12 +6,17 @@ int main(){
     char arr[100];
     scanf("%s", arr);
     int len = strlen(arr);
+    char even[100];
+    int index = 0;
 
-    for(int i = 0; i < len; i++){
-        if(i % 2 == 1){
-            printf("%c", arr[len-i]);
-        }
+    for(int i = 1; i < len; i+=2){
+        even[index++] = arr[i];
     }
 
+    for(int i = index - 1; i >= 0; i--){
+        printf("%c", even[i]);
+    }
+
+    
     return 0;
 }
