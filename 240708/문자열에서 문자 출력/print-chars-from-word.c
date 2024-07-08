74907bc16f2f288605c9b448d86650bb6f98964c
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-08 19:37:30 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240708/문자열에서 문자 출력/print-chars-from-word.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char arr[10];
+    scanf("%s", arr);
+
+    int len = strlen(arr);
+
+    for(int i = 0; i < len; i++){
+        printf("%c\n", arr[i]);
+    }
+
+    return 0;
+}
