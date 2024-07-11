0c5a1879bebb2ee0ce2b7e2918c5289f306e064f
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-11 20:47:09 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240711/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool Prime(int num);
+int Prime_sum(int a, int b);
+
+int main(){
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", Prime_sum(a, b));
+    
+
+    return 0;
+}
+
+bool Prime(int num){
+    if(num <= 1){
+        return false;
+    }
+    if(num == 2){
+        return true;
+    }
+    if(num % 2 == 0){
+        return false;
+    }
+    for(int i = 3; i*i <= num; i+=2){
+        if(num % i == 0){
+            return false;
+        }
+        return true;
+    }
+}
+
+int Prime_sum(int a, int b){
+    int sum = 0;
+    for(int i = a; i <= b; i++){
+        if(Prime(i)){
+            sum += i;
+        }
+    }
+    return sum;
+}
