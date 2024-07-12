56853b9b5078a58f02c40841495f9a8a576f6a5f
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-12 16:46:00 +0900

    Title: 2021년 날짜의 유무, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240712/2021년 날짜의 유무/with-or-without-2021.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int Max_date(int m);
+bool Date(int m, int d);
+
+int main(){
+
+    int m, d;
+    scanf("%d %d", &m, &d);
+
+    if(Date(m, d)){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    
+    return 0;
+}
+
+int Max_date(int m){
+    int max;
+    if(m == 2){
+        max = 28;
+    }
+    else if(m == 4 || m == 6 || m == 9 || m == 11){
+        max = 30;
+    }
+    else{
+        max = 31;
+    }
+
+    return max;
+}
+
+bool Date(int m, int d){
+    if(d > Max_date(m)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
