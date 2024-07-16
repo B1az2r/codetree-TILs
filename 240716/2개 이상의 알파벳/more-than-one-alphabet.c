7edf723452d58bfddf8ada1fce8e0198a1be2f1f
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-16 19:40:13 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/2개 이상의 알파벳/more-than-one-alphabet.c
+++ 240716/2개 이상의 알파벳/more-than-one-alphabet.c
@@ -2,16 +2,13 @@
 #include <string.h>
 #include <stdbool.h>
 
-int countDifferentCharacters(char *arr);
+bool Compare(char *arr);
 
 int main(){
-
     char arr[100];
     scanf("%s", arr);
 
-    int num = countDifferentCharacters(arr);
-    
-    if(num >= 2){
+    if(Compare(arr)){
         printf("Yes");
     }
     else{
@@ -20,21 +17,17 @@ int main(){
 
     
 
-
     return 0;
 }
 
-int countDifferentCharacters(char *arr) {
-    int charCount[26] = {0};  // 알파벳 빈도수를 저장하는 배열
-    int count = 0;
+bool Compare(char *arr){
 
-    for (int i = 0; arr[i] != '\0'; i++) {
-        int index = arr[i] - 'a';  // 알파벳 'a'를 0으로 하는 인덱스 계산
-        if (charCount[index] == 0) {
-            charCount[index]++;
-            count++;
+    int len = strlen(arr);
+
+    for(int i = 0; i < len; i++){
+        if(arr[i] != arr[0]){
+            return true;
         }
     }
-
-    return count;
+    return false;
 }
