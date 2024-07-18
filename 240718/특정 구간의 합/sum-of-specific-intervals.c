2acebef700707f19254998f86d6e1d04b1342248
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-18 20:36:32 +0900

    Title: 특정 구간의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/특정 구간의 합/sum-of-specific-intervals.c
+++ 240718/특정 구간의 합/sum-of-specific-intervals.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
+
 int a[100];
-void result(int *arr, int cnt); // 반환값을 void로 수정
+int result(int start, int end);
 
 int main() {
-    int n, m, i, j;
+    int n, m, i;
     scanf("%d %d", &n, &m);
-    int b[100] = {0}; // 범위 배열 초기화
-
+    
     // 배열 a 입력
     for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
-
-    // 범위 입력
-    for (j = 0; j < m * 2; j += 2) {
-        scanf("%d %d", &b[j], &b[j + 1]);
+    
+    // 범위 입력 및 결과 출력
+    for (i = 0; i < m; i++) {
+        int start, end;
+        scanf("%d %d", &start, &end);
+        printf("%d\n", result(start, end));
     }
-
-    result(b, m * 2); // 범위의 수를 전달
+    
     return 0;
 }
 
-void result(int *arr, int cnt) {
-    int res = 0, j, k;
-
-    for (j = 0; j < cnt; j += 2) {
-        for (k = arr[j]; k <= arr[j + 1]; k++) {
-            if (k > 0 && k <= 100) { // 유효한 인덱스 범위 내에서 접근
-                res += a[k - 1];
-            }
-        }
-        printf("%d\n", res);
-        res = 0;
+int result(int start, int end) {
+    int sum = 0;
+    for (int i = start - 1; i <= end - 1; i++) {
+        sum += a[i];
     }
+    return sum;
 }
