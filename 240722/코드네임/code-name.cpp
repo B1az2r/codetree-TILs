5c48a81266a9e8bd4647cad676e2d176bbcd3afa
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-22 17:37:28 +0900

    Title: 코드네임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/코드네임/code-name.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Agent{
+    public:
+    char codename;
+    int score;
+
+    Agent() {}
+
+    Agent(char codename, int score){
+        this->codename = codename;
+        this->score = score;
+    }
+};
+
+
+int main(){
+
+    char codename;
+    int score;
+
+    Agent agents[5];
+
+    for(int i = 0; i < 5; i++){
+        cin >> codename >> score;
+        agents[i] = Agent(codename, score);
+    }
+
+    char min_codename;
+    int min;
+
+
+    for(int i = 0; i < 5; i++){
+        min = agents[0].score;
+        min_codename = agents[0].codename;
+
+        if(min > agents[i].score){
+            min = agents[i].score;
+            min_codename = agents[i].codename;
+        }
+    }
+
+    cout << min_codename << " " << min << endl;
+
+
+    return 0;
+}
