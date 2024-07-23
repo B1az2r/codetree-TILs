3b7e361489b595c2626cf9f5276a4021d0c30b60
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-23 18:19:56 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/개인정보/personal-info.cpp
+++ 240723/개인정보/personal-info.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <iomanip> // 추가: 소수점 이하 자리수 설정을 위한 헤더 파일
 
 using namespace std;
 
 class Student {
-    public:
-
+public:
     string name;
     int height;
     double weight;
 
-    Student () {}
-    Student(string name, int height, double weight){
+    Student() {}
+    Student(string name, int height, double weight) {
         this->name = name;
         this->height = height;
         this->weight = weight;
     }
 };
 
-bool Cmp1(const Student &a, const Student &b){
+bool Cmp1(const Student &a, const Student &b) {
     return a.name < b.name;
 }
 
-bool Cmp2(const Student &a, const Student &b){
+bool Cmp2(const Student &a, const Student &b) {
     return a.height > b.height;
 }
 
-Student students [5];
+Student students[5];
 
-int main(){
-
-    for(int i = 0; i < 5; i++){
+int main() {
+    for (int i = 0; i < 5; i++) {
         string name;
         int height;
         double weight;
-        
+
         cin >> name >> height >> weight;
         students[i] = Student(name, height, weight);
     }
@@ -44,8 +43,9 @@ int main(){
 
     cout << "name" << endl;
 
-    for(int i = 0; i < 5; i++){
-        cout << students[i].name << " " << students[i].height << " " << students[i].weight << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << students[i].name << " " << students[i].height << " " 
+             << fixed << setprecision(1) << students[i].weight << endl;
     }
     cout << endl;
 
@@ -53,10 +53,10 @@ int main(){
 
     cout << "height" << endl;
 
-    for(int i = 0; i < 5; i++){
-        cout << students[i].name << " " << students[i].height << " " << students[i].weight << endl;
-    }    
-
+    for (int i = 0; i < 5; i++) {
+        cout << students[i].name << " " << students[i].height << " " 
+             << fixed << setprecision(1) << students[i].weight << endl;
+    }
 
     return 0;
 }
