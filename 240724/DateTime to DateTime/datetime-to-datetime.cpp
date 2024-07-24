fa2b7724ad1638104445b381f0b51311723a86c5
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-24 16:55:04 +0900

    Title: DateTime to DateTime, Time: 1096ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240724/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int now_day = 11;
+    int now_hour = 11;
+    int now_min = 11;
+
+    if(a <= now_day){
+        if(b <= now_hour){
+            if(c < now_min){
+                cout << "-1" << endl;
+                return 0;
+            }
+        }
+    }
+
+    int min = 0;
+
+    while(true){
+        
+        if(now_day == a && now_hour == b  && now_min == c){
+            break;
+        }
+
+        min++;
+        now_min++;
+
+        if(now_min == 60){
+            now_hour++;
+            now_min = 0;
+        }
+
+        if(now_hour >= 24){
+            now_day++;
+            now_hour = 0;
+        }
+    }
+    cout << min;
+
+    return 0;
+}
