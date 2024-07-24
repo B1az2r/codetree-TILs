db2db4bf3f7a90398496be57a2e2a7fd479ad45c
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-07-24 19:17:38 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/진수 to 진수/transformation-of-number-system.cpp
+++ 240724/진수 to 진수/transformation-of-number-system.cpp
@@ -1,41 +1,36 @@
 #include <iostream>
-#include <string>
-#include <cmath>
+#include <algorithm>
+
 using namespace std;
 
-// 함수: a진수 문자열을 10진수 정수로 변환
-int convertToDecimal(string n, int a) {
-    int decimal = 0;
-    int length = n.size();
-    for (int i = 0; i < length; i++) {
-        decimal += (n[length - 1 - i] - '0') * pow(a, i);
-    }
-    return decimal;
-}
+int main(){
 
-// 함수: 10진수 정수를 b진수 문자열로 변환
-string convertFromDecimal(int decimal, int b) {
-    string result = "";
-    while (decimal > 0) {
-        result = char((decimal % b) + '0') + result;
-        decimal /= b;
-    }
-    return result;
-}
-
-int main() {
     int a, b;
     string n;
-    cin >> a >> b >> n;
+    int arr[32];
 
-    // 1단계: a진수 문자열 n을 10진수 정수로 변환
-    int decimal = convertToDecimal(n, a);
+    cin >> a >> b;
+    cin >> n;
 
-    // 2단계: 10진수 정수를 b진수 문자열로 변환
-    string result = convertFromDecimal(decimal, b);
+    int num = 0;
+    int cnt = 0;
 
-    // 결과 출력
-    cout << result << endl;
+    for(int i = 0; i < n.size(); i++){
+        num = num * a + (n[i] - '0');
+    }
+
+    while(true){
+        if(num < b){
+            arr[cnt++] = num;
+            break;
+        }
+        arr[cnt++] = num % b;
+        num /= b;
+    }
+
+    for(int i = cnt - 1; i >= 0; i--){
+        cout << arr[i];
+    }
 
     return 0;
 }
