7e47c3f31161ee0d702e27d84f8b945a5b617948
B1az2r <127405284+B1az2r@users.noreply.github.com>
2024-08-15 21:03:04 +0900

    Title: 최대로 겹치는 지점, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240815/최대로 겹치는 지점/maximum-overlapped-points.cpp
+++ 240815/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -1,34 +1,31 @@
 #include <iostream>
 using namespace std;
 
-#define max_n 101
-
-int start[101];
-int ending[101];
+int x1[101];
+int x2[101];
+int check[101];
 
 int main(){
 
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++){
-        int x1, x2;
-        cin >> x1 >> x2;
-        start[x1]++;
-        ending[x2]++;
+    for(int i = 1; i <= n; i++){
+        cin >> x1[i] >> x2[i];
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = x1[i]; j <= x2[i]; j++){
+            check[j]++;
+        }
     }
 
     int max = 0;
-    int current = 0;
 
     for(int i = 1; i <= 100; i++){
-        current += start[i];
-
-        if(current > max){
-            max = current;
+        if(check[i] > max){
+            max = check[i];
         }
-
-        current -= ending[i];
     }
 
     cout << max;
